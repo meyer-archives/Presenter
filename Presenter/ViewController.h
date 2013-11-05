9a0fd2591db07210f1dc0b7716e82cfdb5c1d597
Mike Meyer <meyer@workbook.corp.dropbox.com>
2013-11-05 11:08:03 -0800

    Initial Commit

---
+++ Presenter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Presenter
+//
+//  Created by Mike Meyer on 05/11/2013.
+//  Copyright (c) 2013 Mike Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Presenter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Presenter
+//
+//  Created by Mike Meyer on 05/11/2013.
+//  Copyright (c) 2013 Mike Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
